328b2c03bc36f293003c200bb5ce8d9c3ff87bb3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-30 14:28:20 +0200

    Add deploy

---
+++ .publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && \
+  [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "Deploying image to docker hub"
+  docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USERNAME}"
+  docker push "camptocamp/docker-agent:${TRAVIS_TAG}"
+else
+  echo "Not deploying image"
+fi
