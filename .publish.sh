eeb03ef2ab655bd9081bdc20bfd16a458c5ca84e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-30 14:33:45 +0200

    Pass email to docker login

--- .publish.sh
+++ .publish.sh
@@ -2,7 +2,7 @@
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && \
   [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub"
-  docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USERNAME}"
+  docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USERNAME}" -e "${DOCKER_EMAIL}"
   docker push "camptocamp/docker-agent:${TRAVIS_TAG}"
 else
   echo "Not deploying image"
