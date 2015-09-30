6a9e5c8bfbc680f691adf078c296357f46f42612
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-09-30 14:44:57 +0200

    Add .dockercfg.enc

--- .publish.sh
+++ .publish.sh
@@ -2,7 +2,8 @@
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && \
   [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub"
-  docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USERNAME}" -e "${DOCKER_EMAIL}"
+  # Setup login
+  openssl aes-256-cbc -K $encrypted_7cc4e6238d18_key -iv $encrypted_7cc4e6238d18_iv -in .dockercfg.enc -out ~/.dockercfg -d
   docker push "camptocamp/docker-agent:${TRAVIS_TAG}"
 else
   echo "Not deploying image"
